0dbcff2ba210333707a05a240884e038c4be18f1
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2023-11-09 14:27:30 +0100

    used share GUI approach

--- NoiseCaptureCommon/src/androidMain/kotlin/org/noise_planet/noisecapture/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package org.noise_planet.noisecapture
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- NoiseCaptureCommon/src/commonMain/kotlin/org/noise_planet/noisecapture/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.noise_planet.noisecapture
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
